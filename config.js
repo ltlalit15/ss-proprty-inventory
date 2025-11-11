8bc3c3f276722eee4e2e4f11028ba020873f370f
Gautam bairagi <109797346+GautamBairagi@users.noreply.github.com>
2025-11-11 14:49:53 +0530

    Update database configuration for Railway MySQL

--- config.js
+++ config.js
@@ -1,42 +1,33 @@
 // local mysql
 
-const mysql = require('mysql2/promise');
-
-const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'new_ss_property_inventory',
-    multipleStatements: true
-});
-
- 
-console.warn('DataBase Connected Successfully ss property');
-
-module.exports = db;
-
-
-// live server
-
-
-
 // const mysql = require('mysql2/promise');
 
 // const db = mysql.createPool({
-//     host: 'turntable.proxy.rlwy.net',
-//     port: 49229,
+//     host: '127.0.0.1',
+//     port: 3306,
 //     user: 'root',
-//     password: 'uDgydrXReLDVobOSPJFiIrFEkExNShjv',
-//     database: 'railway',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
+//     password: '',
+//     database: 'new_ss_property_inventory',
+//     multipleStatements: true
 // });
-
-// console.warn('✅ Connected to Railway MySQL');
-
+// console.warn('DataBase Connected Successfully ss property');
 // module.exports = db;
+// live server
+
+
+const mysql = require('mysql2/promise');
+const db = mysql.createPool({
+    host: 'hopper.proxy.rlwy.net',
+    port: 28132,
+    user: 'root',
+    password: 'llSVXJrdrLBLODrhpXBDnrplWUHTdeog',
+    database: 'railway',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+console.warn('✅ Connected to Railway MySQL');
+module.exports = db;
 
 
 
